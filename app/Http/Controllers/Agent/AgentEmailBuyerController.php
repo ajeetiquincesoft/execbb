acd7242b9c0650329057dc2b13e8e1e8203486e8
Santosh <santosh3257@gmail.com>
2024-12-04 11:19:09 +0530

    agent delete issue fix

--- app/Http/Controllers/Agent/AgentEmailBuyerController.php
+++ app/Http/Controllers/Agent/AgentEmailBuyerController.php
@@ -22,11 +22,19 @@ class AgentEmailBuyerController extends Controller
             'subject' => 'required|string|max:255',
             'email_content' => 'required|string',
         ]);
+         // Ensure content from CKEditor is clean and not altered
+         $content = html_entity_decode($request->email_content);  // Decode HTML entities
 
+         // Prepare email data
+         $emailData = [
+             'subject' => $request->subject,
+             'email_content' => $content, // Raw HTML content from CKEditor
+             'recipientEmail' => $request->recipient_email,
+         ];
         // Send the email
         //Mail::to($request->recipientEmail)->send(new BuyerEmail($request->all()));
         foreach ($request->recipientEmail as $email) {
-            Mail::to( $email)->send(new BuyerEmail($request->all()));
+            Mail::to( $email)->send(new BuyerEmail($emailData));
         }
 
         // Return a response (optional)
