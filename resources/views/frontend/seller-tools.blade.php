f0c77b2532c685c82b8264637e370c40cf3f2c26
Santosh <santosh3257@gmail.com>
2025-01-10 18:03:08 +0530

    responsive

--- resources/views/frontend/seller-tools.blade.php
+++ resources/views/frontend/seller-tools.blade.php
@@ -13,34 +13,36 @@
         </nav>
     </div>
 </div>
-<div class="container main-container">
-    <!-- Main Header -->
-    <h1>Educated Clients Make Buying Easier</h1>
-    <p class="subtext">
-    EBB believes purchases are made easier when clients understand the process. This section identifies some of the issues that you will need to address and the <a href="#">sources of anxiety</a> that the buyer may experience.
-    </p>
-    <hr>
-    <!-- Cards Section -->
-    <div class="card-container">
-        <!-- Card 1 -->
-        <div class="card-custom">
-            <div class="card-title">Preparing For the Sale</div>
-            <div class="card-text">
-            Before you think about selling your business, <a href="#">make sure you are prepared.</a> There are three phases to the selling process: 1) Getting Ready, 2) Finding Buyers, 3) Negotiating & Closing the Deal. 
+<div class="container my-7">
+    <div class="main-container">
+        <!-- Main Header -->
+        <h1>Educated Clients Make Buying Easier</h1>
+        <p class="subtext">
+            EBB believes purchases are made easier when clients understand the process. This section identifies some of the issues that you will need to address and the <a href="#">sources of anxiety</a> that the buyer may experience.
+        </p>
+        <hr>
+        <!-- Cards Section -->
+        <div class="card-container">
+            <!-- Card 1 -->
+            <div class="card-custom">
+                <div class="card-title">Preparing For the Sale</div>
+                <div class="card-text">
+                    Before you think about selling your business, <a href="#">make sure you are prepared.</a> There are three phases to the selling process: 1) Getting Ready, 2) Finding Buyers, 3) Negotiating & Closing the Deal.
+                </div>
             </div>
-        </div>
-        <!-- Card 2 -->
-        <div class="card-custom">
-            <div class="card-title">Determining Price</div>
-            <div class="card-text">
-            There are many methods of <a href="{{route('business.valuation')}}" target="_blank">establishing the value of a business.</a> You may want to identify which method or combination of methods you believe would be most appropriate to value your business.
+            <!-- Card 2 -->
+            <div class="card-custom">
+                <div class="card-title">Determining Price</div>
+                <div class="card-text">
+                    There are many methods of <a href="{{route('business.valuation')}}" target="_blank">establishing the value of a business.</a> You may want to identify which method or combination of methods you believe would be most appropriate to value your business.
+                </div>
             </div>
-        </div>
-        <!-- Card 3 -->
-        <div class="card-custom">
-            <div class="card-title">Five Mistakes Sellers Make</div>
-            <div class="card-text">
-            Watch out! Avoid the <a href="#">five most common mistakes</a> sellers make, which include: 1) Improper/incomplete documentation, 2) dealing with only one buyer, 3) not preparing an exit plan, 4) not knowing the value of your business, and 5) not seeking professional advice.
+            <!-- Card 3 -->
+            <div class="card-custom">
+                <div class="card-title">Five Mistakes Sellers Make</div>
+                <div class="card-text">
+                    Watch out! Avoid the <a href="#">five most common mistakes</a> sellers make, which include: 1) Improper/incomplete documentation, 2) dealing with only one buyer, 3) not preparing an exit plan, 4) not knowing the value of your business, and 5) not seeking professional advice.
+                </div>
             </div>
         </div>
     </div>
