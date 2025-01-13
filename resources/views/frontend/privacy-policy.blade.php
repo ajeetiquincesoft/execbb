f7b25927a32c6c1adf26e91bbd0099900e02c8f4
Santosh <santosh3257@gmail.com>
2025-01-13 12:20:11 +0530

    responsive css update

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -20,7 +20,7 @@
             <h1 class="fw-bold">Privacy Policy</h1>
         </div>
         <hr class="pursuit_hr mb-5">
-        <div class="row mb-5 px-5">
+        <div class="row mb-5 px-3 px-md-5">
             <div class="col-md-12">
                 <p class="faq_answer">Executive Business Brokers Inc. is the owner of this site and wishes to express that your privacy is of the utmost concern to us. We guarantee to treat any information received from you with the highest level of <a href="https://www.perfake.me/" class="sellorgive" target="_blank">Rolex Replica Watches</a> respect and confidentiality. The information submitted to us will be used primarily for the purpose of matching your business purchase needs with current or new businesses listed with Executive Business Brokers and its affiliates and for providing products and services related to the buying and selling of a business. Any information received is necessary to provide a level of certainty of interest and to assist in your qualifying. It is also needed in order to aid with the continual improvement of the site, compile basic demographic <a href="https://www.perfake.me/" class="sellorgive" target="_blank">Replica Watches</a> data and to generally improve the services offered to you.
                     The company will not sell or rent your confidential information to any independent third parties, except for Executive Business Brokers partners that provide products or services related to buying and selling a business. In addition, we may share your information with the business seller in which you specifically requested additional information concerning their business.</p>
@@ -30,7 +30,7 @@
         <div class="text-center mb-5 general_qus">
             <h1 class="fw-bold">Cookies</h1>
         </div>
-        <div class="row mb-5 px-5">
+        <div class="row mb-5 px-3 px-md-5">
             <div class="col-md-12">
                 <p class="faq_answer">If you decide to register as a buyer with www.Executivebusinessbrokers.com, "cookies" are used to recognize you on subsequent visits and to make your online experience more user-friendly.<a href="https://www.perfake.me/" class="sellorgive" target="_blank">Best Replica Watches</a> "Cookies" are small bits of data that can be sent to your computer which then may be stored by your browser on your computer's hard drive. They cannot be used to retrieve data from your hard drive, email addresses or personally identifying information about you in any way. Most web browsers have features that can notify you when you receive a "cookie" or prevent "cookies" from being sent. Please note that by not accepting cookies, you will limit the functionality that can be provided to you when you visit www.Executivebusinessbrokers.com.</p>
             </div>
@@ -39,7 +39,7 @@
         <div class="text-center mb-5 general_qus">
             <h1 class="fw-bold">Security</h1>
         </div>
-        <div class="row mb-5 px-5">
+        <div class="row mb-5 px-3 px-md-5">
             <div class="col-md-12">
                 <p class="faq_answer">Transaction information can be securely transmitted to the payment gateway via secure SSL connections.</p>
             </div>
@@ -48,7 +48,7 @@
         <div class="text-center mb-5 general_qus">
             <h1 class="fw-bold">Sponsor Sites</h1>
         </div>
-        <div class="row mb-5 px-5">
+        <div class="row mb-5px-3 px-md-5">
             <div class="col-md-12">
                 <p class="faq_answer">www.Executivebusinessbrokers.com provides links to third party sponsors. These sponsors are independent of this site and regulated by their own policies and procedures.
                     If you have any questions or concerns regarding privacy, please do not hesitate to contact us at admin@Executivebusinessbrokers.com and expect a prompt response to any issues you may have.</p>
