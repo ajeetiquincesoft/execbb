cfd06ed035e1e34d22a63ef45786b70da96cbe56
Santosh <santosh3257@gmail.com>
2025-03-12 12:23:56 +0530

    fix css issue

--- resources/views/vendor/pagination/new_custom.blade.php
+++ resources/views/vendor/pagination/new_custom.blade.php
@@ -1,3 +1,4 @@
+<!-- resources/views/vendor/pagination/custom.blade.php -->
 @if ($paginator->hasPages())
     <div class="pagination-wrapper">
         <ul class="pagination">
@@ -18,16 +19,9 @@
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
-                        {{-- If the page is more than 5 pages away, add ellipsis --}}
-                        @if ($page == 1 || $page == $paginator->lastPage() || $page == $paginator->currentPage() || 
-                            ($page <= $paginator->currentPage() + 2 && $page >= $paginator->currentPage() - 2))
-                            <li class="{{ $page == $paginator->currentPage() ? 'active' : '' }}">
-                                <a href="{{ $url }}" class="rounded">{{ $page }}</a>
-                            </li>
-                        @elseif ($page == 2 || $page == $paginator->lastPage() - 1)
-                            {{-- Display the ellipsis for skipped pages --}}
-                            <li class="disabled"><span>...</span></li>
-                        @endif
+                        <li class="{{ $page == $paginator->currentPage() ? 'active' : '' }}">
+                            <a href="{{ $url }}" class="rounded">{{ $page }}</a>
+                        </li>
                     @endforeach
                 @endif
             @endforeach
@@ -35,7 +29,7 @@
             {{-- Next Page Link --}}
             <li class="{{ $paginator->hasMorePages() ? '' : 'disabled' }}">
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next">
-                    Next &rsaquo; <!-- Right arrow (Next) -->
+                   Next &rsaquo; <!-- Right arrow (Next) -->
                 </a>
             </li>
         </ul>
