05f47b8f561f6d17bac8ec32827503b585e458ea
santosh <santosh3257@gmail.com>
2026-02-24 14:51:28 +0530

    fix view single listing date formate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,8 +18,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(UpdateStatus::class)->everyMinute();
-        $schedule->command(MatchBuyersAndListingsStoredProcedure::class)->everyMinute();
+        $schedule->command(UpdateStatus::class)->dailyAt('14:55');
+        $schedule->command(MatchBuyersAndListingsStoredProcedure::class)->dailyAt('14:55');
         /* $schedule->command('email:test')->everyMinute(); */
     }
 
