8ca37629536daf31eb5b49e9ca0f830b558f284b
santosh <santosh3257@gmail.com>
2026-05-15 14:35:27 +0530

    testing home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,8 @@ class HomeController extends Controller
 {
     public function index()
     {
+        echo 'test';
+        die();
         $states = DB::table('states')->orderByRaw("
         CASE 
             WHEN State = 'nj' THEN 1
